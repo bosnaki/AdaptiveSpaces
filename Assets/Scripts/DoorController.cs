1366f2cca2d17e51349a6be5c18e50b7cf11699c
bosnaki <109136973+bosnaki@users.noreply.github.com>
2026-01-08 01:14:27 +0200

    Door UI

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
 
-public class DoorController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class DoorController : MonoBehaviour {
+    [SerializeField] private Transform player;
+    [SerializeField] private float activationRadius = 3f;
+    [SerializeField] private float checkInterval = 0.1f; // seconds
+    [SerializeField] private GameObject openedDoor1;
+    [SerializeField] private GameObject openedDoor2;
+    [SerializeField] private GameObject openedTile1;
+    [SerializeField] private GameObject openedTile2;
+
+
+    private float radiusSqr;
+    private bool isInside = false;
+
+    private void Awake() {
+        radiusSqr = activationRadius * activationRadius;
+        InvokeRepeating(nameof(CheckDistance), 0f, checkInterval);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void CheckDistance() {
+        Vector2 diff = (Vector2)player.position - (Vector2)transform.position;
+        float distanceSqr = diff.sqrMagnitude;
+
+        if (!isInside && distanceSqr <= radiusSqr) {
+            isInside = true;
+            OnEnterRadius();
+        }
+        else if (isInside && distanceSqr > radiusSqr) {
+            isInside = false;
+            OnExitRadius();
+        }
+    }
+
+    private void OnEnterRadius() {
+        Debug.Log("Entered radius");
+        openedDoor1.SetActive(true);
+        openedDoor2.SetActive(true);
+        openedTile1.SetActive(true);
+        openedTile2.SetActive(true);
+    }
+
+    private void OnExitRadius() {
+        Debug.Log("Exited radius");
+        openedDoor1.SetActive(false);
+        openedDoor2.SetActive(false);
+        openedTile1.SetActive(false);
+        openedTile2.SetActive(false);
+    }
+
+    private void OnValidate() {
+        radiusSqr = activationRadius * activationRadius;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, activationRadius);
     }
 }
